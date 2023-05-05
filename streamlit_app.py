f659c2dea7563b412069bf1657b4b8325c3fabfb
Ramachandra <98381110+gupta7893@users.noreply.github.com>
2023-05-05 16:24:56 +0530

    Update streamlit_app.py
    Added text file by using pandas module and created as a dataframe.

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,9 @@ s.text('🥑🍞 Avocado Toast')
 
 s.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # s.help()
+
+
+import pandas as pd
+
+my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+s.dataframe(my_fruit_list)
