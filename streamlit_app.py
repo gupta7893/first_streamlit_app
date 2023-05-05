393dee417e4185925fb5ad1b8f10fa1f28a0e4b8
Ramachandra <98381110+gupta7893@users.noreply.github.com>
2023-05-05 16:21:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,4 +6,6 @@ s.text('🥣 Omega 3 & Blueberry Oat meal')
 s.text('🥗 Kale, Spinach & Rocket Smoothie')
 s.text('🐔 Hard-bolied Free-range Egg')
 s.text('🥑🍞 Avocado Toast')
+
+s.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 # s.help()
